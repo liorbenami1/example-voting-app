e9fade4170dabaa5b572d9e75b5a6bc18072c0b0
Lior Ben Ami <liorbenami1@gmail.com>
2021-06-19 22:51:46 +0300

    fix

--- vars/readCurrentVersion.groovy
+++ vars/readCurrentVersion.groovy
@@ -8,7 +8,7 @@ def call() {
     sh "ls -l ${env.WORKSPACE}/dev-values.yaml"
     echo "Print dev-values.yaml"
 
-    datas = readYaml file: "${env.WORKSPACE}/dev-values.yaml"
+    def datas = readYaml file: '"${env.WORKSPACE}"/dev-values.yaml'
     //Yaml parser = new Yaml()
     //List example = parser.load(("${env.WORKSPACE}/dev-values.yaml" as File).text)
 
