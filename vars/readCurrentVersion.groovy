03087891233806abc8f2013dc0f888b3c82cfdea
Lior Ben Ami <liorbenami1@gmail.com>
2021-06-19 22:31:37 +0300

    fix

--- vars/readCurrentVersion.groovy
+++ vars/readCurrentVersion.groovy
@@ -7,10 +7,10 @@ import org.yaml.snakeyaml.Yaml
 def call() {
     sh "ls -l ${env.WORKSPACE}/dev-values.yaml"
     echo "Print dev-values.yaml"
-    
-    Yaml parser = new Yaml()
-    List example = parser.load(("${env.WORKSPACE}/dev-values.yaml" as File).text)
 
-    example.each{println it.subject}
+    //Yaml parser = new Yaml()
+    //List example = parser.load(("${env.WORKSPACE}/dev-values.yaml" as File).text)
+
+    //example.each{println it.subject}
 }
 
